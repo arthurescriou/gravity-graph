88772e725a5ec66b4a5f866f47bbb893b710c73f
arthur <arthur.escriou@gmail.com>
2019-10-30 14:12:31 +0100

    change values

--- src/values.js
+++ src/values.js
@@ -1,11 +1,11 @@
 const FRICTION = 0.1
 const K = 1
 const DISTMIN = 200
-const A = 0.000001
-const NEGLICT = 0.1
+const A = 0.00001
+const NEGLICT = 0.01
 
-const NODESSIZE = 60
-const EDGESSIZE = 90
+const NODESSIZE = 12
+const EDGESSIZE = 26
 
 export {
   FRICTION,
